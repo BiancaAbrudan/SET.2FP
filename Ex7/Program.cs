24248c87057c8e06f8db43b69608b358097dd132
BiancaAbrudan <146921015+BiancaAbrudan@users.noreply.github.com>
2023-12-28 16:56:14 +0200

    Add files via upload

---
+++ Ex7/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace Ex7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Introduceti lungimea secventei (n): ");
+            int n = int.Parse(Console.ReadLine());
+
+            int maxim = int.MinValue; 
+            int minim = int.MaxValue; 
+
+            for (int i = 0; i < n; i++)
+            {
+                Console.Write($"Introduceti numarul {i + 1}: ");
+                int numar = int.Parse(Console.ReadLine());
+
+                
+                if (numar > maxim)
+                {
+                    maxim = numar;
+                }
+
+               
+                if (numar < minim)
+                {
+                    minim = numar;
+                }
+            }
+
+            Console.WriteLine($"Cea mai mare valoare din secventa: {maxim}");
+            Console.WriteLine($"Cea mai mică valoare din secventa: {minim}");
+        }
+    }
+}
