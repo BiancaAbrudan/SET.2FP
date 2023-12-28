d0c6e96e5fe83d6f3007c12c6050780e7498ebc9
BiancaAbrudan <146921015+BiancaAbrudan@users.noreply.github.com>
2023-12-28 17:00:24 +0200

    Add files via upload

---
+++ Ex13/Program.cs
@@ -0,0 +1,47 @@
+﻿namespace Ex13
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Introduceti lungimea secventei: ");
+            int n = int.Parse(Console.ReadLine());
+
+            int stareaAnterioara = 0; 
+            bool esteCrescatoareRotita = true;
+
+            for (int i = 0; i < n; i++)
+            {
+                Console.Write($"Introduceti numarul {i + 1}: ");
+                int numar = int.Parse(Console.ReadLine());
+
+                if (i > 0)
+                {
+                    if (numar < stareaAnterioara)
+                    {
+                        esteCrescatoareRotita = false;
+                        break;
+                    }
+                    else if (numar > stareaAnterioara)
+                    {
+                        stareaAnterioara = numar;
+                    }
+                }
+                else
+                {
+                    stareaAnterioara = numar;
+                }
+            }
+
+            if (esteCrescatoareRotita)
+            {
+                Console.WriteLine("Secventa este o secventa crescatoare rotita.");
+            }
+            else
+            {
+                Console.WriteLine("Secventa nu este o secventa crescatoare rotita.");
+            }
+        }
+    }
+    
+}
