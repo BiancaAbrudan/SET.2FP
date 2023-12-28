5e0ce84d88607e07223507e3967706f05eb7e4ce
BiancaAbrudan <146921015+BiancaAbrudan@users.noreply.github.com>
2023-12-28 16:58:20 +0200

    Add files via upload

---
+++ Ex10/Program.cs
@@ -0,0 +1,41 @@
+﻿namespace Ex10
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Introduceti lungimea secventei (n): ");
+            int n = int.Parse(Console.ReadLine());
+
+            int lungimeMaxima = 1; 
+            int lungimeCurenta = 1;
+
+            int numarPrecedent = 0; 
+
+            for (int i = 1; i <= n; i++)
+            {
+                Console.Write($"Introduceti numarul {i}: ");
+                int numar = int.Parse(Console.ReadLine());
+
+                if (numar == numarPrecedent)
+                {
+                    lungimeCurenta++;
+                }
+                else
+                {
+                    lungimeCurenta = 1; 
+                }
+
+                if (lungimeCurenta > lungimeMaxima)
+                {
+                    lungimeMaxima = lungimeCurenta;
+                }
+
+                numarPrecedent = numar;
+            }
+
+            Console.WriteLine($"Numarul maxim de numere consecutive egale din secventa este: {lungimeMaxima}");
+        }
+    }
+    
+}
