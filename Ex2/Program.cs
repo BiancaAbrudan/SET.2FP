0888ad7e2dc113dd9f82d740cb315d29a2aa8574
BiancaAbrudan <146921015+BiancaAbrudan@users.noreply.github.com>
2023-12-28 16:51:20 +0200

    Add files via upload

---
+++ Ex2/Program.cs
@@ -0,0 +1,41 @@
+﻿
+namespace Ex2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("dati secventa de numere (n): ");
+            int n = int.Parse(Console.ReadLine());
+
+            int negativeCount = 0;
+            int zeroCount = 0;
+            int positiveCount = 0;
+
+            for (int i = 0; i < n; i++)
+            {
+                Console.Write($"Introduceti numarul {i + 1} : ");
+                int num = int.Parse(Console.ReadLine());
+
+                if (num < 0)
+                {
+                    negativeCount++;
+                }
+                else
+                if (num == 0)
+                {
+                    zeroCount++;
+                }
+                else
+                {
+                    positiveCount++;
+                }
+            }
+            Console.WriteLine($"Numere negative: {negativeCount}");
+            Console.WriteLine($"Numere zero: {zeroCount}");
+            Console.WriteLine($"Numere pozitive {positiveCount}");
+
+                
+        }
+    }
+}
