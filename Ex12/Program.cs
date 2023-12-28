e3df1eb1c8bc6603fb647deee8f5f896ca69eec5
BiancaAbrudan <146921015+BiancaAbrudan@users.noreply.github.com>
2023-12-28 16:59:39 +0200

    Add files via upload

---
+++ Ex12/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace Ex12
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Introduceti lungimea secventei: ");
+            int n = int.Parse(Console.ReadLine());
+
+            int stareaAnterioara = 0; 
+            int numarGrupuri = 0;
+
+            for (int i = 0; i < n; i++)
+            {
+                Console.Write($"Introduceti numarul {i + 1}: ");
+                int numar = int.Parse(Console.ReadLine());
+
+                if (numar != 0)
+                {
+                    if (stareaAnterioara == 0)
+                    {
+                        numarGrupuri++;
+                        stareaAnterioara = 1;
+                    }
+                }
+                else
+                {
+                    stareaAnterioara = 0;
+                }
+            }
+
+            Console.WriteLine($"Numarul de grupuri de numere consecutive diferite de zero este: {numarGrupuri}");
+        }
+    }
+}
