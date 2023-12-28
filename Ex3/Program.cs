a847b2c85846e99a639e868b10f73b113b47a467
BiancaAbrudan <146921015+BiancaAbrudan@users.noreply.github.com>
2023-12-28 16:53:03 +0200

    Add files via upload

---
+++ Ex3/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace Ex3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Introduceti numarul (n): ");
+            int n = int.Parse(Console.ReadLine());
+
+            int suma = 0;
+            int produs = 1;
+            for (int i = 0; i < n; i++)
+            {
+                suma += i;
+                produs += i;
+            }
+            Console.WriteLine($"Suma numerelor de la 1 la {n} este: {suma}");
+            Console.WriteLine($"Produsul numerelor de la 1 la {n} este: {produs}"); 
+        }
+    }
+}
