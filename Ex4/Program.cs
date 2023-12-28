e3cea0512678aed274e9fd6f5533c3ca47c52ef6
BiancaAbrudan <146921015+BiancaAbrudan@users.noreply.github.com>
2023-12-28 16:53:56 +0200

    Add files via upload

---
+++ Ex4/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace Ex4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Introduceti lungimea secventei (n): ");
+            int n = int.Parse(Console.ReadLine());
+            Console.WriteLine("Introduceti numarul cautat (a): ");
+            int a = int.Parse(Console.ReadLine());
+            int pozitie = -1;
+            for (int i = 0; i < n; i++)
+            {
+                Console.WriteLine($"Introduceti numarul {i + 1}");
+                int numar = int.Parse(Console.ReadLine());
+
+                if (numar == a)
+                {
+                    pozitie = i;
+                    break;
+                }
+            }
+            Console.WriteLine($"Numarul a se afla pe pozitia: {pozitie}");
+        }
+    }
+}
