885ec513938a8cb676dc579ad66d3ee4edbc5b9f
BiancaAbrudan <146921015+BiancaAbrudan@users.noreply.github.com>
2023-12-28 16:59:00 +0200

    Add files via upload

---
+++ Ex11/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace Ex11
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Introduceti lungimea secventei: ");
+            int n = int.Parse(Console.ReadLine());
+
+            double sumaInverselor = 0;
+
+            for (int i = 0; i < n; i++)
+            {
+                Console.Write($"Introduceti numarul {i + 1}: ");
+                double numar = double.Parse(Console.ReadLine());
+
+                sumaInverselor += 1 / numar;
+            }
+
+            Console.WriteLine($"Suma inverselor este: {sumaInverselor}");
+        }
+    }
+}
