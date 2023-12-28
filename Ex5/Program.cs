d5da271aee1044aa0d7ff522c2289d20f64535ea
BiancaAbrudan <146921015+BiancaAbrudan@users.noreply.github.com>
2023-12-28 16:54:53 +0200

    Add files via upload

---
+++ Ex5/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace Ex5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Introduceti lungimea secventei (n): ");
+            int n = int.Parse(Console.ReadLine());
+
+            int count = 0;
+
+            for (int i = 0; i < n; i++)
+            {
+                Console.Write($"Introduceti numarul {i}: ");
+                int numar = int.Parse(Console.ReadLine());
+
+                if (numar == i)
+                {
+                    count++;
+                }
+            }
+
+            Console.WriteLine($"Numarul de elemente egale cu pozitia lor în secventa este: {count}");
+        }
+    }
+}
