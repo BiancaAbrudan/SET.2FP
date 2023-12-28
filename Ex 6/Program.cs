3c239da8c9f34e494178bd9f49e7a92318a8e5f0
BiancaAbrudan <146921015+BiancaAbrudan@users.noreply.github.com>
2023-12-28 16:55:31 +0200

    Add files via upload

---
+++ Ex 6/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace Ex_6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Introduceti lungimea secventei (n): ");
+            int n = int.Parse(Console.ReadLine());
+
+            bool ordineCrescatoare = true;
+
+            for (int i = 1; i < n; i++)
+            {
+                Console.Write($"Introduceti numarul {i}: ");
+                int numarAnterior = int.Parse(Console.ReadLine());
+
+                Console.Write($"Introduceti numarul {i + 1}: ");
+                int numarCurent = int.Parse(Console.ReadLine());
+
+                if (numarCurent < numarAnterior)
+                {
+                    ordineCrescatoare = false;
+                    break;
+                }
+            }
+
+            if (ordineCrescatoare)
+            {
+                Console.WriteLine("Numerele sunt în ordine crescatoare.");
+            }
+            else
+            {
+                Console.WriteLine("Numerele nu sunt în ordine crescatoare.");
+            }
+        }
+    }
+}
