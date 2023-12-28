c104093d5d6ac7fc0ef12a97515ddbda5c33c8e4
BiancaAbrudan <146921015+BiancaAbrudan@users.noreply.github.com>
2023-12-28 17:01:45 +0200

    Add files via upload

---
+++ Ex15/Program.cs
@@ -0,0 +1,68 @@
+﻿namespace Ex15
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Introduceti lungimea secventei: ");
+            int n = int.Parse(Console.ReadLine());
+
+            int stareaAnterioara = 0;
+            bool esteBitonica = true;
+
+            for (int i = 0; i < n; i++)
+            {
+                Console.Write($"Introduceti numarul {i + 1}: ");
+                int numar = int.Parse(Console.ReadLine());
+
+                if (i > 0)
+                {
+                    if (stareaAnterioara == 0)
+                    {
+                        if (numar > stareaAnterioara)
+                        {
+                            stareaAnterioara = 1;
+                        }
+                        else if (numar < stareaAnterioara)
+                        {
+                            esteBitonica = false;
+                            break;
+                        }
+                    }
+                    else if (stareaAnterioara == 1)
+                    {
+                        if (numar < stareaAnterioara)
+                        {
+                            stareaAnterioara = -1;
+                        }
+                        else if (numar == stareaAnterioara)
+                        {
+                            stareaAnterioara = 0;
+                        }
+                    }
+                    else if (stareaAnterioara == -1)
+                    {
+                        if (numar >= stareaAnterioara)
+                        {
+                            esteBitonica = false;
+                            break;
+                        }
+                    }
+                }
+                else
+                {
+                    stareaAnterioara = 0;
+                }
+            }
+
+            if (esteBitonica)
+            {
+                Console.WriteLine("Secventa este bitonica.");
+            }
+            else
+            {
+                Console.WriteLine("Secventa nu este bitonica.");
+            }
+        }
+    }
+}
